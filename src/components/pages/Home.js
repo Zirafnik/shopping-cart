d6b33bf39a32de1c9bdfc80cd7b05ceb3adaeca9
david <dh@ptica.com>
2021-03-20 13:42:10 +0100

    home page + style

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+
+    return (
+        <div className='home'>
+            <button>Visit The Shop</button>
+        </div>
+    );
+}
+
+export default Home;
